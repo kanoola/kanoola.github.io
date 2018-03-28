874b772ce7482a997538e5c8b024fcd420d4111c
kanoola <kanooladesign@gmail.com>
2018-03-28 14:39:53 -0400

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
        // Set the date we're counting down to
-       var countDownDate = new Date("March 25, 2018 15:37:25").getTime();
+       var countDownDate = new Date(2018,5,19,12,6,0,0).getTime();
        // Update the count down every 1 second
        var x = setInterval(function() {
            // Get todays date and time
